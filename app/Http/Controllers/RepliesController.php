dd9d7072c334374fe65322e896724ecaa70299cb
Harun R Rayhan <harun.b13@gmail.com>
2018-04-14 17:29:48 +0600

    Filter Threads using Channel & Channel View

--- app/Http/Controllers/RepliesController.php
+++ app/Http/Controllers/RepliesController.php
@@ -11,10 +11,18 @@ class RepliesController extends Controller
 		$this->middleware('auth');
 	}
 
+	/**
+	 *
+	 *
+	 * @param $channelId
+	 * @param Thread $thread
+	 *
+	 * @return \Illuminate\Http\RedirectResponse
+	 */
 	public function store($channelId, Thread $thread) {
 
 		$this->validate(request(), ['body' => 'required']);
-		
+
 		$thread->addReply([
 			'body'  => request('body'),
 			'user_id'   => auth()->id()

514a2aa2da16f8b101ba5833de9181df72aaddbe
Harun R Rayhan <harun.b13@gmail.com>
2018-04-15 21:29:09 +0600

    Favorite feature for reply

--- routes/web.php
+++ routes/web.php
@@ -26,3 +26,4 @@ Route::get('/threads/{channel}', 'ThreadsController@index');
 Route::get('/threads/{channel}/{thread}', 'ThreadsController@show');
 //Route::resource('threads', 'ThreadsController');
 Route::post('/threads/{channel}/{thread}/replies', 'RepliesController@store');
+Route::post('/replies/{reply}/favorites', 'FavoritesController@store');

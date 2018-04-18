048e63a46042227f78de76ded99b15d08485a500
Harun R Rayhan <harun.b13@gmail.com>
2018-04-18 20:56:45 +0600

    Added Favorite feature

--- resources/views/threads/reply.blade.php
+++ resources/views/threads/reply.blade.php
@@ -1,10 +1,25 @@
 <div class="card">
 
     <div class="card-header">
-        <a href="#">
-            {{ $reply->owner->name }}
-        </a>
-        said {{ $reply->created_at->diffForHumans() }}...
+        <div class="level">
+            <h5 class="flex">
+                <a href="#">
+                    {{ $reply->owner->name }}
+                </a>
+                said {{ $reply->created_at->diffForHumans() }}...
+            </h5>
+            <div>
+                {{ $reply->favorites()->count() }}
+                <form method="POST" action="/replies/{{ $reply->id }}/favorites">
+                    {{ csrf_field() }}
+                    <button type="submit" class="btn btn-default btn-primary" {{ $reply->isFavorited() ? 'disabled' : ''  }}>
+                        {{ $reply->favorites()->count() }}  {{ str_plural('Favorite', $reply->favorites()->count()) }}
+                    </button>
+                </form>
+            </div>
+
+
+        </div>
     </div>
 
     <div class="card-body">
